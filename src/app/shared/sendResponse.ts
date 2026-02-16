16b5899495e51957288b9894f4777ac2447f9e2d
abujaher5 <abujaher5120@gmail.com>
2026-02-16 17:26:15 +0600

    setup prisma schema in a moduler format

--- src/app/shared/sendResponse.ts
+++ src/app/shared/sendResponse.ts
@@ -1,3 +1,11 @@
+import { Response } from "express";
+
+interface IResponseData<T> {
+  httpStatusCode: number;
+  success: boolean;
+  message: string;
+  data?: T;
+}
 export const sendResponse = <T>(
   res: Response,
   responseData: IResponseData<T>,
