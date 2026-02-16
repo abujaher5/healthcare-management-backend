03782430785cb0dde73618a9f73b27abfe516752
abujaher5 <abujaher5120@gmail.com>
2026-02-16 23:08:37 +0600

    implement better-auth schema and auth.ts

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,43 @@
+import { betterAuth, boolean } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import { Role, UserStatus } from "../../generated/prisma/enums";
+// If your Prisma file is located elsewhere, you can change the path
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: true,
+        defaultValue: Role.PATIENT,
+      },
+      status: {
+        type: "string",
+        returned: true,
+        defaultValue: UserStatus.ACTIVE,
+      },
+      needPasswordChange: {
+        type: "boolean",
+        required: true,
+        defaultValue: false,
+      },
+      isDeleted: {
+        type: "boolean",
+        required: true,
+        defaultValue: false,
+      },
+      deletedAt: {
+        type: "date",
+        returned: false,
+        defaultValue: null,
+      },
+    },
+  },
+});
