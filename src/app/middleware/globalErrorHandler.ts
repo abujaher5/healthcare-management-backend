6720286e0f8f7b61dd5dfd6bc7e15412fea03513
abujaher5 <abujaher5120@gmail.com>
2026-04-06 15:38:14 +0600

    implement zod middleware and zod error handling

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -2,6 +2,9 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../../config/env";
 import status from "http-status";
+import z from "zod";
+import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
+import { handleZodError } from "../errorHelpers/handleZodError";
 
 export const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -14,12 +17,25 @@ export const globalErrorHandler = (
     console.log("Error from Global Error Handler", err);
   }
 
-  const statusCode: number = status.INTERNAL_SERVER_ERROR;
-  const message: string = "Internal Server Error";
+  let errorSources: TErrorSources[] = [];
 
-  res.status(statusCode).json({
+  let statusCode: number = status.INTERNAL_SERVER_ERROR;
+  let message: string = "Internal Server Error";
+
+  if (err instanceof z.ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode as number;
+    message = "Zod Validation Error";
+
+    errorSources = [...simplifiedError.errorSources];
+  }
+
+  const errorResponse: TErrorResponse = {
     success: false,
     message: message,
-    error: err.message,
-  });
+    errorSources,
+    error: envVars.NODE_ENV === "development" ? err : undefined,
+  };
+
+  res.status(statusCode).json(errorResponse);
 };
